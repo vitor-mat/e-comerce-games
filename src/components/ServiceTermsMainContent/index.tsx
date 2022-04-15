c7811ca97a135d76579782cd756fee5cc2aa102b
vitor-mat <vitormateusalves123@gmail.com>
2022-04-14 23:04:03 -0300

    add service terms page

---
+++ src/components/ServiceTermsMainContent/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import * as S from './style';
+
+export const ServiceTermsMainContent: React.FC = () => {
+  return (
+    <S.Container>
+      <h1>service terms</h1>
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac massa
+        convallis nulla dignissim pellentesque. Donec commodo lectus iaculis,
+        finibus massa eu, laoreet leo. Nullam dui sapien, mattis faucibus
+        lobortis et, aliquam ac nibh. Curabitur mattis suscipit vulputate. Aenean
+        et tincidunt sem, ut congue urna. Nullam scelerisque hendrerit orci, vel
+        tincidunt ipsum accumsan nec. Curabitur nec quam purus. Maecenas sapien
+        purus, elementum vitae hendrerit nec, tempus eget lorem. Donec ac diam in
+        tellus placerat condimentum vel ut nisl. Ut posuere nunc ut sem rhoncus
+        convallis. Proin sagittis ultricies ornare. Nam viverra lorem ex, a
+        facilisis magna dictum sed. Mauris nunc metus, tristique mollis diam id,
+        commodo elementum urna. Quisque eros eros, luctus eget quam non, pretium
+        vestibulum est. Vestibulum ante ipsum primis in faucibus orci luctus et
+        ultrices posuere cubilia curae; Nulla dolor justo, auctor a libero ac,
+        sodales finibus nunc.
+
+        Phasellus venenatis velit id consectetur fringilla. Donec venenatis vestibulum
+        mauris, ut varius velit auctor et. Etiam scelerisque felis et varius tristique.
+        Duis a lectus diam. Quisque sed tellus massa. Vestibulum ante ipsum primis in
+        faucibus orci luctus et ultrices posuere cubilia curae; Proin volutpat nisi
+        vitae placerat mollis. Quisque et erat nibh. Pellentesque habitant morbi
+        tristique senectus et netus et malesuada fames ac turpis egestas. Orci varius
+        natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        Etiam finibus felis eget ex maximus, eget vestibulum magna consequat.
+
+        Nullam consequat ac metus eu faucibus. Aenean vitae tellus sed magna consectetur
+        luctus. Ut vel est sed sem sollicitudin feugiat. Nunc porta auctor cursus. Etiam
+        aliquam aliquam lectus, vel scelerisque diam finibus eget. Nunc erat turpis,
+        dignissim fringilla felis vitae, ultrices porta enim. Vestibulum lacus mauris,
+        gravida vitae ante vitae, viverra euismod urna. Nam rutrum lectus feugiat nulla
+        fermentum, sed scelerisque massa tempor. Sed scelerisque accumsan ante. Vivamus
+        porttitor libero non dignissim consequat. Vivamus condimentum rhoncus sapien, ut
+        cursus tellus ullamcorper non. In hac habitasse platea dictumst. Maecenas
+        ullamcorper nec orci in placerat.
+      </p>
+    </S.Container>
+  )
+}
