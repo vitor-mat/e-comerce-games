a94fde22a47e7021bad21f8cf4b6a4d90c60d4a1
vitor-mat <vitormateusalves123@gmail.com>
2022-04-15 10:31:24 -0300

    add redux config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import { store } from '../app/store';
+import { Provider } from 'react-redux';
+
 import { GlobalStyle } from '../styles/globalStyle';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   )
 }
 
