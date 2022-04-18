b8571f68a14438b8cdd846f88355167d1e3c6a64
vitor-mat <vitormateusalves123@gmail.com>
2022-04-18 08:33:49 -0300

    add test - describe(adding product s in the cart) - checking if the respective game is rendered

---
+++ src/__testes__/games.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
+
+import { store } from '../app/store';
+import { Provider } from 'react-redux';
+
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../styles/theme';
+
+import Games from '../pages/games/[id]';
+
+
+function MockGames() {
+  return(
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Games currentlyRouter="terra-media-sombras-de-mordor"/>
+      </ThemeProvider>
+    </Provider>
+  )
+}
+
+describe("adding products in the cart", () => {
+  test("checking if the respective game is rendered", async () => {
+    const { debug } = render(<MockGames />)
+    const title = screen.getByText("Terra Média: Sombras de Mordor")
+    expect(title).toBeInTheDocument()
+  })
+})
