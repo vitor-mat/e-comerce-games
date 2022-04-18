852862ff4e877c9d56d5c3163f9afc55038bf0b3
vitor-mat <vitormateusalves123@gmail.com>
2022-04-18 06:47:00 -0300

    change project language + fix bug on the _document page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,9 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang="pt-br">
         <Head>
+          <title>Supera Games</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
@@ -33,12 +34,12 @@ export default class MyDocument extends Document {
       const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
-        styles: (
+        styles: [
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        ),
+        ],
       }
     } finally {
       sheet.seal()
